9d5a66fe16cf8a7170400e96d1dfa6fc684a8128
W35113Y <128330604+W35113Y@users.noreply.github.com>
2026-06-21 21:30:07 -0300

    Add files via upload

---
+++ lista03.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double valor;
+
+        do
+        {
+            Console.Write("Digite um valor positivo: ");
+            valor = Convert.ToDouble(Console.ReadLine());
+
+            if (valor <= 0)
+                Console.WriteLine("Erro! Digite apenas valores positivos.");
+        }
+        while (valor <= 0);
+
+        Console.WriteLine($"Valor informado: {valor}");
+    }
+}
