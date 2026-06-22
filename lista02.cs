4e6b6a39c85d933ccf2003dba964a71fa2294add
W35113Y <128330604+W35113Y@users.noreply.github.com>
2026-06-21 21:16:33 -0300

    Add files via upload

---
+++ lista02.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double p1, p2Necessaria;
+
+        Console.Write("Digite a nota P1: ");
+        p1 = Convert.ToDouble(Console.ReadLine());
+
+        p2Necessaria = (15 - p1) / 2;
+
+        Console.WriteLine($"Necessita tirar {p2Necessaria:F2} na P2 para aprovação.");
+
+        if (p2Necessaria > 10)
+            Console.WriteLine("Mesmo tirando 10 não será possível atingir a média.");
+    }
+}
