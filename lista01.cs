fd0bb6c195345b85b78a739515153929d7f95525
W35113Y <128330604+W35113Y@users.noreply.github.com>
2026-06-21 21:05:20 -0300

    Add files via upload

---
+++ lista01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a, b, media;
+
+        Console.Write("Digite o primeiro valor: ");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo valor: ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        media = Math.Sqrt(a * b);
+
+        Console.WriteLine($"Média geométrica: {media}");
+    }
+}
